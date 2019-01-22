ca42a53d5cf4b13edb99aabb8bdde7288cc665e2
Rob Benton <rmbenton@arkbluecross.com>
2019-01-22 10:57:27 -0600

    Added a component

--- src/main/java/com/rob/wickettest/WicketApplication.java
+++ src/main/java/com/rob/wickettest/WicketApplication.java
@@ -2,7 +2,6 @@ package com.rob.wickettest;
 
 import com.rob.wickettest.page.Home;
 import org.apache.wicket.Page;
-import org.apache.wicket.core.util.file.WebApplicationPath;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
@@ -16,16 +15,5 @@ public class WicketApplication extends WebApplication
     public Class<? extends Page> getHomePage()
     {
         return Home.class;
-    }
-
-    @Override
-    protected void init()
-    {
-        super.init();
-        /*
-        getResourceSettings().getResourceFinders().add(
-            new WebApplicationPath(getServletContext(), TEMPLATE_PATH)
-        );
-        */
     }
 }
--- src/main/java/com/rob/wickettest/page/Home.java
+++ src/main/java/com/rob/wickettest/page/Home.java
@@ -1,10 +1,15 @@
 package com.rob.wickettest.page;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 
 /**
  * @author Rob Benton
  */
 public class Home extends WebPage
 {
+    public Home()
+    {
+        add(new Label("message", "Wicket sucks :)"));
+    }
 }
