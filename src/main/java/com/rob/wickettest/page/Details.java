cdfd05015bae5eb5d79a465aa74535c7ba065466
Rob Benton <rmbenton@arkbluecross.com>
2019-01-23 08:22:07 -0600

    Made a link to a details page

---
+++ src/main/java/com/rob/wickettest/page/Details.java
@@ -0,0 +1,24 @@
+package com.rob.wickettest.page;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+
+import java.util.Date;
+
+/**
+ * @author Rob Benton
+ */
+public class Details extends WebPage
+{
+    private static final String VERSION_ID = "version";
+    private static final String GEN_DATE_ID = "generatedDate";
+
+    private final String version;
+
+    public Details()
+    {
+        version = getClass().getPackage().getImplementationVersion();
+        add(new Label(VERSION_ID, version));
+        add(new Label(GEN_DATE_ID, new Date()));
+    }
+}
--- src/main/java/com/rob/wickettest/page/Home.java
+++ src/main/java/com/rob/wickettest/page/Home.java
@@ -2,14 +2,30 @@ package com.rob.wickettest.page;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 
 /**
  * @author Rob Benton
  */
 public class Home extends WebPage
 {
+    private static final String DETAILS_ID = "details";
+
     public Home()
     {
+        // Add text labels.
         add(new Label("message", "Wicket message :)"));
+
+        // Add links.
+        add(
+            new Link<Void>(DETAILS_ID)
+            {
+                @Override
+                public void onClick()
+                {
+                    setResponsePage(Details.class);
+                }
+            }
+        );
     }
 }
