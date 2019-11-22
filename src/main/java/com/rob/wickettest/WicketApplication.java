81ab7769c31f01c367b1db059a4769c0ab591183
Rob Benton <rob.benton7@gmail.com>
2019-11-22 12:42:38 -0600

    Added bean validation.

--- src/main/java/com/rob/wickettest/WicketApplication.java
+++ src/main/java/com/rob/wickettest/WicketApplication.java
@@ -2,6 +2,7 @@ package com.rob.wickettest;
 
 import com.rob.wickettest.page.HomePage;
 import org.apache.wicket.Page;
+import org.apache.wicket.bean.validation.BeanValidationConfiguration;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
@@ -13,5 +14,13 @@ public class WicketApplication extends WebApplication
 	public Class<? extends Page> getHomePage()
 	{
 		return HomePage.class;
+	}
+
+	@Override
+	public void init()
+	{
+		super.init();
+
+		new BeanValidationConfiguration().configure(this);
 	}
 }
