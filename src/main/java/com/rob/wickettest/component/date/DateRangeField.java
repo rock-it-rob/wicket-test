4c861d55cf49de995ee6e17a77b6a81fb2115bf5
Rob Benton <rob.benton7@gmail.com>
2020-05-21 13:34:00 -0500

    Created a custom component for a date range.

--- src/main/java/com/rob/wickettest/component/date/DateRangeField.java
+++ src/main/java/com/rob/wickettest/component/date/DateRangeField.java
@@ -1,27 +1,41 @@
 package com.rob.wickettest.component.date;
 
-import org.apache.wicket.ajax.AjaxEventBehavior;
+import com.rob.wickettest.page.AbstractPage;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.markup.html.form.FormComponentPanel;
+import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
+import org.apache.wicket.feedback.ComponentFeedbackMessageFilter;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.validation.IFormValidator;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.validation.ValidationError;
 import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 
-public class DateRangeField extends FormComponentPanel<DateRangeField.DateRangeModel>
+public class DateRangeField extends Panel
 {
     private static final Logger log = LoggerFactory.getLogger(DateRangeField.class);
 
+    private static final String FORM_ID = "form";
     private static final String START_DATE_ID = "startDate";
     private static final String END_DATE_ID = "endDate";
+    private static final String START_DATE_FEEDBACK_ID = "startDateFeedback";
+    private static final String END_DATE_FEEDBACK_ID = "endDateFeedback";
 
+    private final Form<Void> form = new Form<>(FORM_ID);
     private final TextField<LocalDate> startDateInput = new TextField<>(START_DATE_ID);
     private final TextField<LocalDate> endDateInput = new TextField<>(END_DATE_ID);
+    private final FeedbackPanel startDateFeedback = new FeedbackPanel(START_DATE_FEEDBACK_ID, new ComponentFeedbackMessageFilter(startDateInput));
+    private final FeedbackPanel endDateFeedback = new FeedbackPanel(END_DATE_FEEDBACK_ID, new ComponentFeedbackMessageFilter(endDateInput));
 
     public DateRangeField(String id, IModel<DateRangeModel> model)
     {
@@ -34,29 +48,84 @@ public class DateRangeField extends FormComponentPanel<DateRangeField.DateRangeM
     {
         super.onInitialize();
 
-        startDateInput.setLabel(new PropertyModel<>(getDefaultModel(), "startDateLabel"));
-        endDateInput.setLabel(new PropertyModel<>(getDefaultModel(), "endDateLabel"));
-
-        final String event = "blur";
-        add(new AjaxEventBehavior(event)
+        form.setOutputMarkupId(true);
+        form.add(new IFormValidator()
         {
             @Override
-            protected void onEvent(AjaxRequestTarget target)
+            public FormComponent<?>[] getDependentFormComponents()
             {
-                log.info(event);
+                return new FormComponent[]{startDateInput, endDateInput};
+            }
+
+            @Override
+            public void validate(Form<?> form)
+            {
+                DateRangeField.this.validate(startDateInput.getConvertedInput(), endDateInput.getConvertedInput(), startDateInput);
             }
         });
 
-        add(startDateInput);
-        add(endDateInput);
+        startDateInput.setLabel(new PropertyModel<>(getDefaultModel(), "startDateLabel"));
+        startDateInput.add(new AjaxFormSubmitBehavior("blur")
+        {
+            @Override
+            protected void onError(AjaxRequestTarget target)
+            {
+                final AbstractPage page = (AbstractPage) getPage();
+                target.add(startDateFeedback);
+            }
+        });
+
+        endDateInput.setLabel(new PropertyModel<>(getDefaultModel(), "endDateLabel"));
+        endDateInput.add(new AjaxFormSubmitBehavior("blur")
+        {
+            @Override
+            protected void onError(AjaxRequestTarget target)
+            {
+                final AbstractPage page = (AbstractPage) getPage();
+                target.add(endDateFeedback);
+            }
+        });
+
+        startDateFeedback.setOutputMarkupId(true);
+        endDateFeedback.setOutputMarkupId(true);
+
+        add(form);
+        form.add(startDateInput);
+        form.add(endDateInput);
+        form.add(startDateFeedback);
+        form.add(endDateFeedback);
+    }
+
+    private void validateDateRange()
+    {
+        final LocalDate start = startDateInput.getConvertedInput();
+        final LocalDate end = endDateInput.getConvertedInput();
+        final DateRangeModel dateRangeModel = (DateRangeModel) getDefaultModelObject();
+
+        if (start.compareTo(end) > 0)
+        {
+            startDateInput.error(new ValidationError(String.format("%s must be on or before %s", dateRangeModel.getStartDateLabel(), dateRangeModel.getEndDateLabel())));
+        }
+    }
+
+    /**
+     * Override for custom date range validation.
+     *
+     * @param start         start date.
+     * @param end           end date.
+     * @param formComponent component to register validation errors against.
+     */
+    protected void validate(LocalDate start, LocalDate end, FormComponent<?> formComponent)
+    {
+        validateDateRange();
     }
 
     public static class DateRangeModel implements Serializable
     {
         private LocalDate startDate;
         private LocalDate endDate;
-        private String startDateLabel;
-        private String endDateLabel;
+        private String startDateLabel = "Start Date";
+        private String endDateLabel = "End Date";
 
         public DateRangeModel()
         {
--- src/main/java/com/rob/wickettest/page/ComplexFormComponentPage.java
+++ src/main/java/com/rob/wickettest/page/ComplexFormComponentPage.java
@@ -1,6 +1,8 @@
 package com.rob.wickettest.page;
 
 import com.rob.wickettest.component.date.DateRangeField;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.PropertyModel;
 import org.joda.time.LocalDate;
@@ -9,6 +11,7 @@ public class ComplexFormComponentPage extends AbstractPage
 {
     private static final String FORM_ID = "form";
     private static final String DATERANGE_ID = "dateRangeField";
+    private static final String SUBMIT_BUTTON_ID = "submit";
 
     private final DateRangeField.DateRangeModel dateRangeModel = new DateRangeField.DateRangeModel(new LocalDate(), new LocalDate());
 
@@ -18,9 +21,28 @@ public class ComplexFormComponentPage extends AbstractPage
         super.onInitialize();
 
         final Form<Void> form = new Form<>(FORM_ID);
+        form.setOutputMarkupId(true);
         add(form);
 
         final DateRangeField dateRangeField = new DateRangeField(DATERANGE_ID, new PropertyModel<>(this, "dateRangeModel"));
         form.add(dateRangeField);
+
+        final AjaxButton submitButton = new AjaxButton(SUBMIT_BUTTON_ID)
+        {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form)
+            {
+                success("Saved");
+                target.add(feedbackPanel);
+                target.add(form);
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target, Form<?> form)
+            {
+                target.add(feedbackPanel);
+            }
+        };
+        form.add(submitButton);
     }
 }
